7473096bc5c4add7773e8cb99d5e883314104a13
Oka <45233151+CharlesVitry@users.noreply.github.com>
2023-01-27 19:17:48 +0100

    push

--- borne_sup.py
+++ borne_sup.py
@@ -1,19 +1,23 @@
+from donnees import import_donnees_from_json
+import numpy as np
 
 def borne_lagrangienne(D):
     i = 0
     itermax = 5
 
     # coefs relaxation
-    alpha = 0.9
-    ro = 2
+    α = 0.9
+    ρ = 2
 
-    # multiplicateur de lagrange
-    l, u = 0
+    len(D.Articles) * len(lots)
+    # vecteurs multiplicateur de lagrange
+    λ, μ = np.zeros()
 
     while i < itermax:
         # Résolution du sous-problème Lagrangien 𝐒𝐏𝐋𝟏(λ, 𝜇) → 𝑥𝑖𝑘(̅̅̅̅̅ λ, 𝜇)
         # xik = nombre d’articles de type i impliqué dans les lots de type k
         # couts régus       
+
 
         # Résolution du sous-problème Lagrangien 𝐒𝐏𝐋𝟐(λ) → 𝑦𝑖𝑘(̅̅̅̅̅ λ)
         # yik = 1 si l’article i est constitutif d’un lot de type k, 0 sinon
@@ -34,8 +38,26 @@ def borne_lagrangienne(D):
         i += 1
 
         # révision coef de relaxation
-        ro = alpha * ro
+        ρ = α * ρ
 
+
+def lots_possibles(D):
+    # Lots possibles
+    lots = list(chain(*map(lambda x: combinations(D.Articles, x),
+                range(D.e_min, D.e_max + 1))))
+    print("Nombre de lots possibles : " + str(len(lots)))
+    
+    # Lots avec un indice commercial suffisant
+    lots = list(filter(lambda x: sum(
+        [article.indice for article in x]) >= D.r_min, lots))
+    print("Nombre de lots avec un indice commercial suffisant : " + str(len(lots)))
+
+    # Trie par profit net décroissant/ nombre d'articles
+    lots = sorted(lots, key=lambda x: len(
+        x) / (sum([article.prix for article in x]) - D.cd * len(x)))
+    
+    lots = [list(lot) for lot in lots]
+    return lots
 
 instance = "instances_test/test1.json"
 D = import_donnees_from_json(instance)
